41e5cfa1c8b585b5f23d6e4052a14f317e0099cb
fidelnyame <nyamefidel7@gmail.com>
2026-05-04 16:08:48 +0000

    new changes

--- src/main/main.ts
+++ src/main/main.ts
@@ -57,6 +57,7 @@ function createWindow() {
     mainWindow.webContents.openDevTools()
   } else {
     mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'))
+    mainWindow.webContents.openDevTools()
   }
 
   mainWindow.on('closed', () => {
